ee4a1d86a99a1ecc1b0e182e97d027e3f8a29483
Antoine <antoine.guerin@ynov.com>
2023-06-14 15:51:47 +0200

    feat: setup atomicité

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,6 +3,9 @@ import { useCookies } from "react-cookie";
 import AUTH from "../data/auth";
 import { getBoardsUser } from "../services/boardServices";
 import { Boards } from "../data/boards";
+import {H1, H2, H3} from './styles/atoms/Titles';
+
+
 
 const Home = () => {
     const [cookies] = useCookies(['access_token', 'refresh_token'])
@@ -21,13 +24,13 @@ const Home = () => {
 
 
     return (
-        <div className="container">
-            <h1>Mes tableaux</h1>
-            <div>
+        <div className="container" id="home-container">
+            <H1 color="red">Mes tableaux</H1>
+            <div className="all-boards">
                 {data?.board.map((board) => (
-                    <div key={board.board.id}>
-                        <a href={"/board/" + board.board.id}>
-                            <h3>{board.board.title}</h3>
+                    <div key={board.board.id} className="boards" >
+                        <a href={"/board/" + board.board.id} className="link-to-board">
+                            <H3>{board.board.title}</H3>
                             <p>{board.board.hash}</p>
                             <p>{board.role}</p>
                         </a>
@@ -36,6 +39,7 @@ const Home = () => {
             </div>
         </div>
     );
+
 };
 
 export default Home;
---
+++ src/components/styles/atoms/Titles.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export const H1 = styled.h1`
+background-color: ${props=>props.color};
+`
+
+export const H2 = styled.h2`
+background-color: ${props=>props.color};
+`
+
+export const H3 = styled.h3`
+background-color: ${props=>props.color};
+`
